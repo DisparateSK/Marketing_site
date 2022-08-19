51e1ce48f6e31a8a4453d5daab9f4f6aa212912c
Sergey <102255416+DisparateSK@users.noreply.github.com>
2022-08-19 23:15:44 +0300

    test getting data from tg bot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, json, render_template
+from flask import Flask, json, render_template, jsonify, redirect
 from flask import request
 from flask import Response
 import requests
@@ -247,6 +247,30 @@ def index():
         return render_template("index.html")
 
 
+@app.route('/login')
+def login():
+    tg_data = {
+        "id": request.args.get('id', None),
+        "first_name": request.args.get('first_name', None),
+        "last_name": request.args.get('last_name', None),
+        "username": request.args.get('username', None),
+        "photo_url": request.args.get('photo_url', None),
+        "auth_date": request.args.get('auth_date', None),
+        "hash": request.args.get('hash', None)
+    }
+    # data_check_string = string_generator(tg_data)
+    # secret_key = hashlib.sha256(app.config['BOT_TOKEN'].encode('utf-8')).digest()
+    # secret_key_bytes = secret_key
+    # data_check_string_bytes = bytes(data_check_string, 'utf-8')
+    # hmac_string = hmac.new(secret_key_bytes, data_check_string_bytes, hashlib.sha256).hexdigest()
+    # if hmac_string == tg_data['hash']:
+    #     return redirect('/dashboard')
+
+    return jsonify({
+        # 'hmac_string': hmac_string,
+        # 'tg_hash': tg_data['hash'],
+        'tg_data': tg_data
+    })
 
 
 if __name__ == '__main__':
